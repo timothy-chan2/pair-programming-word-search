52181be877a9c0d060138a1cd391d52505a1e778
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-11-03 21:30:00 +0000

    Add code to pass all mocha & chai tests in searching for word

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,34 @@
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
+    const lettersT = transpose(letters);
+    if (word === '') {
+        return false;
+    }
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) {
+            return true;
+        }
     }
-}
-
-module.exports = wordSearch
+    const verticalJoin = lettersT.map(ls => ls.join(''))
+    for (l of verticalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+    return false;
+};
+const transpose = function (matrix) {
+    // Put your solution here
+    const len = matrix.length;
+    let newArray = [];
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (!Array.isArray(newArray[j])) {
+                newArray[j] = [];
+            }
+            newArray[j][i] = matrix[i][j];
+        }
+    }
+    return newArray;
+};
+module.exports = wordSearch;
